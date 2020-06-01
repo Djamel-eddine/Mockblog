8b8afefab5bd1a0bd1d79388271c0707922d1309
mahrougui djamel <djamelmahrougui@gmail.com>
2020-06-01 20:06:15 +0100

    adding the error page -404- style

--- src/components/error.js
+++ src/components/error.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import error_image from "./resources/images/404.png";
+import "./style/error.css";
 
 const Error = () => {
-  return <div>404 Error</div>;
+  return (
+    <div className="error-container">
+      <div>
+        <img src={error_image} alt="" />
+        <Link to="/">
+          <button className="btn1">back home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
